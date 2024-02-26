a3fc85a30825761586c6f4e84ffa15a322a5346c
shaikSameerbasha5404 <sameerbasha8125@gmail.com>
2024-02-26 11:27:40 +0530

    hi

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
 ﻿
 using System;
-public class Pattern
+
+class PascalTriangle
 {
     public static void Main()
     {
-        // type the code here
+        Console.Write("Enter the number of rows of Pascal Triangle: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int space = 0; space < rows - i; space++)
+            {
+                Console.Write(" ");
+            }
+
+            int num = 1;
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(num + " ");
+                num = num * (i - j) / (j + 1);
+            }
+            Console.WriteLine();
+        }
     }
 }
